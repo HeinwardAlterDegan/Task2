3c3ac4896697612c4fc52dfa25b6208d6f8807d5
Эдуард Гусев <f1ful1337@gmail.com>
2024-09-30 12:53:42 +0500

    SinCosVer1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            double angleInDegrees = 15;
+            double angleInRadians = angleInDegrees * Math.PI / 180;
+            double sin = Math.Sin(angleInRadians);
+            double cos = Math.Cos(angleInRadians);
+
+
+            Console.WriteLine("sin(15) =" + Math.Round(sin,3));
+            Console.WriteLine("cos(15) =" + Math.Round(cos,3));
+
+            Console.ReadKey();
         }
     }
 }
